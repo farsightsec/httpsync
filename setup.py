984914d5affd21bced6a7f4f7da08affe5e5eba0
Henry Stern <stern@fsi.io>
2014-10-19 06:55:45 -0300

    Version 0.1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='httpsync',
-        version='0.1',
+        version='0.1.1',
         description='HTTP Sync Client',
         author='Henry Stern',
         author_email='stern@fsi.io',
